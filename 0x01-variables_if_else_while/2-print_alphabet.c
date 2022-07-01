81ffb34b89813a562730f13d536550f071f79c5a
cazicruz <dcazicruz@icloud.com>
2022-07-01 15:35:14 -0700

    print alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include < stdio.h >
+
+
+
+
+
+/**
+ * main = Entry point
+ * Return: Always 0 (success)
+ */
+
+
+int main()  
+
+{  
+	char ch = 'a';
+	while(ch <= 'z')
+	{
+		putchar("%c \n", ch);
+		ch++;
+
+
+		return 0;
+	}
+}
