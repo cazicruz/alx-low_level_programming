fb5991f61c207ad67240c96d5ae6c34b52c2af65
cazicruz <dcazicruz@icloud.com>
2022-06-30 20:07:04 -0700

    typo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,23 @@
-#include <stdio.h>;                                                                                               
+#include <stdio.h>
 
-                                                                                                                 
 
-                                                                                                                 
 
-/* main - entery point                                                                                           
+/**
  *
- *  * ruturn : always 0(succes)                                                                                     
+ *  * main - Entry point
  *
- *   */                                                                                                              
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
-                                                                                                                 
+int main(void)
 
-                                                                                                                 
+{
 
-int main(void)                                                                                                   
+		puts("Programming is like building a multilingual puzzle");
 
-{                                                                                                                
+			return (0);
 
-	        puts("Programming is like building a multilingual puzzle\n");                                            
-	        return(0);                                                                                               
-
-};   
+}
