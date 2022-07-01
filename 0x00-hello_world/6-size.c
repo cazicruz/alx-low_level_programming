e3bc58b231dddb36e5d6d4c85944bed1926a8337
cazicruz <dcazicruz@icloud.com>
2022-07-01 02:31:14 -0700

    size of prog

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,15 +7,21 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
-{
+int main(void){
 
-	printf("Size of a char: %lu byte(s)", sizeof(char));
-	printf("Size of int: %lu byte(s)", sizeof(int));
-	printf("Size of a long int: %lu byte(s)", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
+	        int i;
 
+	        long int l;
+	        long long int ll;
+	        float d;
+	        char c;
+		printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	        printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	        printf("Size of a long int: %lu byte(s)\n", sizeof(l));
+	        printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
+	        printf("Size of a float: %lu byte(s)\n", sizeof(d));
+	        return (0);
 
-	return 0;
 }
+
+
