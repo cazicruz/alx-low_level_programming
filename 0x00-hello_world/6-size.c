2e69064877c672f6a8725ad5c4f35aed5a266000
cazicruz <dcazicruz@icloud.com>
2022-07-01 02:26:04 -0700

    size of prog

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  * 
  * Discription: This is a program to print the size of the data types i accordnce with the system type
- *
- *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 
-	printf("Size of a char: %i byte(s)", sizeof(char));
-	printf("Size of int: %i byte(s)", sizeof(int));
-	printf("Size of a long int: %i byte(s)", sizeof(long int));
-	printf("Size of a long long int: %i byte(s)", sizeof(long long int));
-	printf("Size of a float: %i byte(s)", sizeof(float));
+	printf("Size of a char: %lu byte(s)", sizeof(char));
+	printf("Size of int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+
+
 	return 0;
 }
