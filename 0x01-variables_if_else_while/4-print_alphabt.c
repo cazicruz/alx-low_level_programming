652bf793729bee90eb0f8f54de192cfd092c78e3
cazicruz <dcazicruz@icloud.com>
2022-07-02 15:53:47 -0700

    copied task 2 and added an if else

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,7 +15,14 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	putchar (ch);
+	{
+		if (ch != "q" && ch != "e")
+		{
+			putchar (ch);
+		}
+		else
+			ch++;}
+
 	putchar ('\n');
 
 	return (0);
