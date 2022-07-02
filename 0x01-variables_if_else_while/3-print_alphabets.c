82a81c8d35aa65f1d04fe29d1b2c15723768e5e6
cazicruz <dcazicruz@icloud.com>
2022-07-02 14:22:58 -0700

    print alpha A-z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Entry point
+ * Discription: code to print alphabets
+ * Return: Always 0 (success)
+ */
+
+
+
+int main(void)
+{
+	char ch;
+	for (ch = 'A'; ch >= 'z'; ch++)
+		putchar(ch);
+	putchat('\n')
+
+	return (0);
+}
