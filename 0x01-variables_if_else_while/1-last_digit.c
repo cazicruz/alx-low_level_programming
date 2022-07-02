f3e0de360532b49fa519ec1a39639eb6d2edffaf
cazicruz <dcazicruz@icloud.com>
2022-07-02 02:30:43 -0700

    fixed a decleration error

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,16 +22,16 @@ int main(void)
 
 	if (lastnum > 5)
 	{	
-		printf("Last digit of %d and is %d greater "
+		printf("Last digit of %d is %d and is greater "
 				"than 5", n , lastnum);
 	}
 	else if (lastnum == 0)
 	{
-		printf("Last digit of %d and is %d and is 0", n , lastnum);
+		printf("Last digit of %d is %d and is 0", n , lastnum);
 	}
 	else if (lastnum < 6 && lastnum != 0)
 	{
-		printf("Last digit of %d and is %d and is less than 6"
+		printf("Last digit of %d is %d and is less than 6"
 			       " and not 0",n ,lastnum);
 	}
 	return (0);
