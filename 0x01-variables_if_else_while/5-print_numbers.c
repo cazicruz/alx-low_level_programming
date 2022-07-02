4c9889860e35a61e013df71795046bb2af2ad6b4
cazicruz <dcazicruz@icloud.com>
2022-07-02 16:56:49 -0700

    still on time

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,14 @@
  * Return: Always 0 (success)
  */
 
-int main (void)
+int main(void)
 {
 	int num;
 
 	for (num = 0; num <10; num++)
-	{	printf("%i \n", num);
+	{	printf("%i", num);
 	}
+	printf("\n")
+
 	return (0);
 }
