5ec5a5c6752a9a7915634430714992d65cb4ad93
cazicruz <dcazicruz@icloud.com>
2022-07-02 14:04:55 -0700

    print alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,19 +4,19 @@
 
 
 /**
- * main = Entry point
+ * main - Entry point
  * Discription: code to print alphabets
  * Return: Always 0 (success)
  */
 
 
 int main(void)
-{  
+{
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	putchar (ch);
 	putchar ('\n');
 
-	return(0);
+	return (0);
 }
