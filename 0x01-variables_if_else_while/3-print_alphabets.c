0d0142181b7e5d8127f6e255ee49945e12514e7e
cazicruz <dcazicruz@icloud.com>
2022-07-02 15:13:03 -0700

    fixed error

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * Entry point
+ * main - Entry point
  * Discription: code to print alphabets
  * Return: Always 0 (success)
  */
@@ -11,10 +11,15 @@
 int main(void)
 {
 	char ch;
+	char cA;
 
 	for (ch = 'a'; ch <= 'Z'; ch++)
 	{
 		putchar (ch);
+	}
+	for (cA = "A"; cA <= "Z"; cA++)
+	{
+		putchar (cA)
 	}
 	putchar ('\n');
 
