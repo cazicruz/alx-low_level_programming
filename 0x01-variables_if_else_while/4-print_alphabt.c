ceab3a797d7e85dd55c39b28940421852d031103
cazicruz <dcazicruz@icloud.com>
2022-07-02 16:25:38 -0700

    found the bug

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,14 +14,14 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 97; ch <= 122; ch++)
+	for (ch = 97; ch <= 122;)
 	{
 		if (ch != 113 && ch != 101)
 		{
 			putchar (ch);
 		}
 		else
-			ch--;
+			ch++;
 	}
 
 	putchar (10);
