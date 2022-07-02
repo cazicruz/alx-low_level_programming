db89fa27274f6ea8173c145c57146eb719c6399c
cazicruz <dcazicruz@icloud.com>
2022-07-02 16:48:03 -0700

    first try

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * decision: try to make this work in first attempt
+ * Return: Always 0 (success)
+ */
+
+int main (void)
+	int num;
+
+	for (num = 0; num <10; num++)
+	{	printf("%i \n", num)
+	}
