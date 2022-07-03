90719ef773de9b5a2465a55b1ad85af5a3afb202
cazicruz <dcazicruz@icloud.com>
2022-07-02 18:42:04 -0700

    2combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * print a list of combined numbers
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+	int n;
+	int l
+
+	for (ch = 48; ch <= 58; ch++)
+	{
+		for (n = 49; n <= 57; n++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > n && n > ch)
+				{
+					putchar(ch);
+					putchar(n);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				
+				}
+			}
+		}
+	}
+	putchar(10);  /*this is ascii code for  new line*/
+
+	return (0);
+}
