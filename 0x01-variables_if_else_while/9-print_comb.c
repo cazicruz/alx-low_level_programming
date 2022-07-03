9ddf4e6a711a2fc0686a5d184f9d6ce9a8bbc26d
cazicruz <dcazicruz@icloud.com>
2022-07-02 17:35:27 -0700

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * decision: try to make this work in first attempt
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; num++)
+	{	putchar(num);
+	}
+	putchar(10);
+
+	return (0);
+}
