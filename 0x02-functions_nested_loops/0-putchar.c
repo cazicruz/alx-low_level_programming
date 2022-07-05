2800f4680771c64da84ae911fe8663ec2acdd05d
cazicruz <dcazicruz@icloud.com>
2022-07-05 12:52:56 -0700

    a file to print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <main.h>
 /*
  * main - Entry point
  * Return: 0 (success)
  */
 
-int main(void)
+int print putchar(void)
 {
-	printf("_putchar");
+	putchar("_putchar");
 	return (0);
 }
