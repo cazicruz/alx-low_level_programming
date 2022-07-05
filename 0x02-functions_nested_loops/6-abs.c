2fa0579fc6a49be099138db4842fcf8702ec56a2
cazicruz <dcazicruz@icloud.com>
2022-07-05 16:01:34 -0700

    absolute value func

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ * @n: this is the input or number to compare
+ * Return: always return 0 (success)
+ */
+
+int _abs(int)
+{
+	int n;
+	if (n > 0)
+	{
+		_putchar(n*(-1));
+	}
+	else if 
+		_putchar(n);
+	return (0)
+}
