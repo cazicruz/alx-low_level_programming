387ce66bdc678b600aaa6ce0225b07dd80c8fc8b
cazicruz <dcazicruz@icloud.com>
2022-07-05 16:27:57 -0700

    get last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ * @c: this is the input or letter to compare
+ * Return: always return 0 (success)
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
