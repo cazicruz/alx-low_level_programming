4f9d1bd08c313dc2b56ee30c5a8ea1c80e15eace
cazicruz <dcazicruz@icloud.com>
2022-07-05 03:21:15 -0700

    prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always return 0 (success)
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{
+	return (0)
+}
+void print_alphabet(void)
+{
+	char ch;
+
+	for(ch = "a"; ch <= "z"; ch++)
+	{
+		printf("%c", ch);
+	}
+	printf("\n");
+}
