6d19a75533baea2c0eff4528b5e237296b22ba81
cazicruz <dcazicruz@icloud.com>
2022-07-06 13:38:12 -0700

    fingers cossed

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
- * add - entry point
- *@x ,y : input param
- * thin is a function that adds two numbers
- * return:always 0 (success)
+ * Add - entry point
+ * @x: input parameter
+ * @y: input parameter
+ * Return: always 0 (success)
  */
 int add(int x, int y)
 {
 	return (x + y);
-}
- 
+} 
