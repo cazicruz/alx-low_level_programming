9a6bbdcfb4167fd0d01be23a8f644a17bf8f8220
cazicruz <dcazicruz@icloud.com>
2022-07-06 14:29:24 -0700

    n-98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,20 +8,16 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n = n; n < 98; n++)
+		for (; n <= 98; n++)
 		{
-			_putchar(n);
-			_putchar(44);
-			_putchar(32);
+			printf("%d, ", n);
 		}
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
 		while (n >= 98)
 		{
-			_putchar(n);
-			_putchar(44);
-			_putchar(32);
+			printf("%d, ", n);
 			n++;
 		}
 	}
