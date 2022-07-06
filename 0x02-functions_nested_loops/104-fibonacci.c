81d9aba262d5c0083541231c51c6880e2534b0eb
cazicruz <dcazicruz@icloud.com>
2022-07-06 16:25:38 -0700

    fibonacci pt2

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * discription: fabonacci
+ * Return: x + y
+ */
+int main(void)
+{
+	int count = 0;
+	long int n = 2;
+	long int m = 1;
+	long int j;
+
+	printf("%ld, %ld, ", m, n);
+
+	for (; count <= 98; count++)
+	{
+		j = n;
+		n = n + m;
+		printf("%ld", n);
+		m = j;
+		if (count != 98)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
