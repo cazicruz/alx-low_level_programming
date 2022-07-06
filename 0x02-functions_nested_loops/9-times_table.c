4e3b4fffb578545508b7a4a51e7afc8dc8ae3227
David Onwuli <dcazicruz@icloud.com>
2022-07-06 15:29:23 -0400

    fingers crossed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,24 +12,29 @@ void times_table(void)
 	int i;
 	int table_1;
 
-	for (tX1 = 0; tX1 <= 9; tX1++)
+	for (tX1 = 0; tX1 < 10; tX1++)
 	{
 		_putchar(48);
-		for  (i = 1; i <= 9; i++)
+		for  (i = 1; i <= 10; i++)
 		{
 			table_1 = tX1 * i;
-			_putchar(44);
-			_putchar(32);
-			/*if (table_1 >= 9)
+			
+			if (i == 0)
+				_putchar(table_1 + '0');
+			if (i != 0 && table_1 < 10)
 			{
-				_putchar(32);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(table_1 + '0');
 			}
-			else
+			else if (table_1 >= 10)
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((table_1 / 10) + 48);
 				_putchar((table_1 % 10) + 48);
-			}*/
+			}
 		}
 		_putchar('\n');
 	}
