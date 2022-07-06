de517bae301b24c61af476f8e9d8eb6c592263e8
David Onwuli <dcazicruz@icloud.com>
2022-07-06 15:30:50 -0400

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,10 +15,9 @@ void times_table(void)
 	for (tX1 = 0; tX1 < 10; tX1++)
 	{
 		_putchar(48);
-		for  (i = 1; i <= 10; i++)
+		for  (i = 1; i < 10; i++)
 		{
 			table_1 = tX1 * i;
-			
 			if (i == 0)
 				_putchar(table_1 + '0');
 			if (i != 0 && table_1 < 10)
