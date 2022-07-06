1c6e1c172b9b3fbc27827f4bbd75f15a2769f988
cazicruz <dcazicruz@icloud.com>
2022-07-06 13:45:01 -0700

    fingers cossed

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * Add - entry point
+ * add -> entry point
  * @x: input parameter
  * @y: input parameter
  * +: used for addition
- * Return: always 0 (success)
+ * Return: x + y
  */
 int add(int x, int y)
 {
 	return (x + y);
-} 
+}
