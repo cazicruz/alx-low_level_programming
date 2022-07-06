c9e816051d1a7c7f3ea253d801e8ab06403fa64e
cazicruz <dcazicruz@icloud.com>
2022-07-06 06:41:04 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * times_table - Entry point
+ * @n: this is the input or number to compare
+ * Return: always return 0 (success)
+ */
+
+void times_table(void)
+{
+	int tX1;
+	for (tX1 = 0; tX1 < 10; tX1++)
+	{
+		for  (i = 0; i < 10; i++)
+		{
+			table_1 = tX1 * i;
+			_putchar("%i ,", table1);
+		}
+	}
+	return (0);
+}
