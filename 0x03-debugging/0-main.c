b7e9efa10c2a57b856e0602a6654bd2d532add32
cazicruz <dcazicruz@icloud.com>
2022-07-07 14:35:10 -0700

    debug finishing

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,6 +3,7 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
+ *@i: input variable
  * Return: 0
  */
 
@@ -10,9 +11,9 @@
 int main(void)
 {
 	int i;
-	
+
 	i = 0;
 	positive_or_negative(i);
-	
+
 	return (0);
 }
