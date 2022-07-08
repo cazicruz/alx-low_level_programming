3dfd7b52a5407e1280618e99338fcdf270b81657
cazicruz <dcazicruz@icloud.com>
2022-07-08 13:25:06 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,21 @@ int main(void)
 	{
 		if (i % 3 = 0)
 		{
-			printf("Fizz ")
+			printf("Fizz ");
 		}
 		else if (i % 5 = 0)
 		{
-			printf("Buzz ")
+			printf("Buzz ");
 		}
 		else if (i % 15 = 0)
 		{
-			printf("FizzBuzz ")
+			printf("FizzBuzz ");
 		}
 		else
 		{
-			printf("%i ", i)
+			printf("%i ", i);
 		}
 	printf('\n');
 	}
+	return (0);
 }
