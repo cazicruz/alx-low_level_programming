251360812d2c5ea23154e2988d2b99a79727f1df
cazicruz <dcazicruz@icloud.com>
2022-07-08 14:08:27 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 /**
  * main - entry point
+ * @i: integer
  * return: Always return 0 (success)
  */
 
@@ -13,15 +14,15 @@ int main(void)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (i == 1)
 		{
@@ -29,7 +30,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%i ", i);
+			printf(" %i", i);
 		}
 	}
 	printf("\n");
