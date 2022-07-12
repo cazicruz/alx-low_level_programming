64bcd6f91d483238eafacadae8b98ebe0a926f05
cazicruz <dcazicruz@icloud.com>
2022-07-12 15:51:45 -0700

    prnt str in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,8 +2,8 @@
 #include "main.h"
 
 /**
- * _puts - Entry point
- * @str: string
+ * print_rev - Entry point
+ * @s: string
  * Return: returns the lenght of the string
  */
 
@@ -13,7 +13,7 @@ void print_rev(char *s)
 	int count = 0;
 	int n;
 
-	while (str[count] != 0)
+	while (s[count] != 0)
 	{
 		count++;
 	}
