84a7c772c0e1acbb3ed3fd7fa0af67c1a1e5623d
cazicruz <dcazicruz@icloud.com>
2022-07-12 07:17:28 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
-/*
+/**
  * reset_to_98 - Entry point
  * Discription: this is a function that changes the value a pointer points to
  * @*n: pointer variable
