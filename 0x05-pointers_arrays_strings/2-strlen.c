c3bde140626e8f9b7f04e6c347a695ec139fdeca
cazicruz <dcazicruz@icloud.com>
2022-07-12 07:51:15 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * Discription: this is a function that swaps two values
+ * @a: pointer variable
+ * @b: pointer variable
+ */
+
+int _strlen(char *s)
+{
+	return (sizeof(*s)-1);
+}
