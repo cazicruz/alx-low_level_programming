2ca31aec5cf202e62aeef8fcf157701f095a33f9
cazicruz <dcazicruz@icloud.com>
2022-07-12 13:33:00 -0700

    print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: string
+ * Return: returns the lenght of the string
+ */
+
+void _puts(char *str)
+{
+	int count = 0;
+	int i;
+
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		_putchar(s[i]);
+		if (s[i] == 0)
+			_putchar('\n');
+	}
+	return (0);
+}
