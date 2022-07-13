a610b5244c953df12b01c4f91baa5e92bf46e848
cazicruz <dcazicruz@icloud.com>
2022-07-13 06:53:43 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: string
+ * Return: returns the lenght of the string
+ */
+
+
+void print_array(int *a, int n)
+{
+	int i = n - 1
+
+	for (i = 0; i < n; i++)
+	{
+		if ((i +1) >= n)
+		{
+			_putchar(a[i]);
+		}
+		else
+		{
+			_putchar(a[i]);
+			_putchar(",");
+			_putchar(" ");
+		}
+
+	}
+	if (i == n)
+		_putchar('\n');
+}
