a7c526a54cd78ef7f7357405bdf2ec8bb288336f
cazicruz <dcazicruz@icloud.com>
2022-07-13 08:20:24 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,25 +11,6 @@
 
 void print_array(int *a, int n)
 {
-	/*int i = n - 1;
-
-	for (i = 0; i < n; i++)
-	{
-		if ((i + 1) >= n)
-		{
-			printf("%i", a[i]);
-			printf("\n");
-		}
-		else if (a[n] != 0)
-		{
-			printf("\n");
-		}
-		else
-		{
-			printf("%i, ", a[i]);
-		}
-
-	}*/
 	int j = 0;
 	for (; j < n; j++)
 	{
