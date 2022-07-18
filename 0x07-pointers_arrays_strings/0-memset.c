28ab4a29b4c5aa79e3be8589e46aeee5e8ba3bcd
cazicruz <dcazicruz@icloud.com>
2022-07-18 13:37:33 -0700

    task 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,9 +3,10 @@
 
 /**
  * _memset - Entry point
+ * @s: memory area
  * @n: number of bytes to fill
  * @b: constant byte
- * return: Always return s 
+ * return: Always return *s 
  */
 
 char *_memset(char *s, char b, unsigned int n)
