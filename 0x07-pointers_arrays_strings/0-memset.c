ab3e0d9180e65d7676033d28dc1ff9bc0cde42d8
cazicruz <dcazicruz@icloud.com>
2022-07-18 13:30:14 -0700

    task 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,6 +3,8 @@
 
 /**
  * *_memset - Entry point
+ *  @n: number of bytes to fill
+ *  @b: constant byte
  * return: Always return 0
  */
 
@@ -10,7 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
