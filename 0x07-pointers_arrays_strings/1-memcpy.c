21788b16febbd3d93b305b8b5fb56ab46d14e193
cazicruz <dcazicruz@icloud.com>
2022-07-18 14:03:06 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memcpy - Entry point
+ * @s: memory area
+ * @n: number of bytes to fill
+ * @b: constant byte
+ * Return: return *s
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (s);
+}
