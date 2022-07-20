ed01d48481e5040e507f8a94ca981ffe4f815ee0
cazicruz <dcazicruz@icloud.com>
2022-07-20 15:56:03 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,37 @@
 #include "main.h"
+
 /**
- * _pow_recursion - Entry point
- * @x: input parameter
- * @y: power to raise x by
- * Return: n!
+ * helper - square root of number
+ * @i: input number
+ * @j: iterator
+ * Return: square root or -1
  */
 
-int _pow_recursion(int x, int y)
+int helper(int i, int j)
 {
-	if (y < 0)
+	if (j % (i / j) == 0)
 	{
+		if (j * (i / j) == i)
+			return (j);
+		else
+			return (-1);
+	}
+	return (0 + helper(i, j + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: the natural square root of a number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
 		return (-1);
-	}
-	else if (y == 0)
-	{
+	if (n == 0)
+		return (0);
+	if (n == 1)
 		return (1);
-	}
-	else
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
+	return (helper(n, 2));
 }
