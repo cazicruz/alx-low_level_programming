c22e4083f4ceda3ad3261f8d7feb79d0b07ac45a
cazicruz <dcazicruz@icloud.com>
2022-07-20 15:03:27 -0700

    task5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - Entry point
+ * @x: input parameter
+ * @y: power to raise x by
+ * Return: n!
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
