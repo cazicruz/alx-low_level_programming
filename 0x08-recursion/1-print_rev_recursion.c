3ced1cd149495d467dc2578cec5d0a89a4410b25
cazicruz <dcazicruz@icloud.com>
2022-07-20 12:43:29 -0700

    task2

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts_recursion - Entry point
+ * @s: input parameter
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
