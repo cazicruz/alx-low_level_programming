c06f833180060347958678c5e03d32603d1617c6
cazicruz <dcazicruz@icloud.com>
2022-07-20 16:12:28 -0700

    task6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * helper - square root of number
+ * @i: input number
+ * @j: iterator
+ * Return: square root or -1
+ */
+
+int primer(int i, int j)
+{
+	if ((i % j) == 0)
+	{
+		if (i == j)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + helper(i, j + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: the natural square root of a number
+ */
+
+int is_prime_number(int n)
+{
+	return (primer(n, 2));
+}
