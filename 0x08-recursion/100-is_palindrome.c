f49d3c57a8f4061dcd1503ff52aa96187856fc16
cazicruz <dcazicruz@icloud.com>
2022-07-20 17:19:51 -0700

    task100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Entry point
+ * @s: input parameter
+ * Return: void
+ */
+
+char _print_rev(*s)
+{
+	char n[100];
+
+	if (*s != '\0')
+	{
+		_print_rev(s + 1);
+		n = *s;
+	}
+	return(n);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number
+ * Return: the natural square root of a number
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == _print_rev(*s))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
