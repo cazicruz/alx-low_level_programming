ec42cb4322b2ff36b96672c25a09d0eee891345e
cazicruz <dcazicruz@icloud.com>
2022-07-22 15:30:54 -0700

    task0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: string
+ * Return: returns the lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+	int i;
+
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		count++;
+	}
+	return (count);
+}
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @c: this is the input or letter to compare
+ * Return: always return 0 (success)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
