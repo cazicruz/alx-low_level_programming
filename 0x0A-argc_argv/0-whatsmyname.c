81f7d94b8ec96f61f1d774ea43885c4220635935
cazicruz <dcazicruz@icloud.com>
2022-07-22 17:36:40 -0700

    argv task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,11 +8,11 @@
  * Return: returns success as 0
  */
 
-int main (int argc, char *argv [])
+int main (int argc, char *argv[])
 {
 	if (argc >= 0)
 	{
-		printf("%s\n", argv [0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
