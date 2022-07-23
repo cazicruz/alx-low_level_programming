1c4b7e8e776fc8b952de5cfb163bbb610f8577d4
cazicruz <dcazicruz@icloud.com>
2022-07-23 04:08:50 -0700

    tasktask

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * main - Entry point of the prog
@@ -11,17 +10,14 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j;
-	if (argc == 3)
-	{
-		i = atoi(argv[1]);
-		j = atoi(argv[2]);
-		printf("%d\n", i * j);
-		return (0);
-	}
-	else
-	{
-		printf("Error");
-		return (1);
-	}
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+}
+return (0);
 }
