0ed726c13b1509a24dfc507891b33ca5a4cdc3da
cazicruz <dcazicruz@icloud.com>
2022-07-22 17:30:06 -0700

    argv task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,6 +3,8 @@
 
 /**
  * Main - Entry point of the prog
+ * @argc: argument count
+ * @argv: argument vector
  * Returns: returns success as 0
  */
 
@@ -12,5 +14,5 @@ int main (int argc, char *argv[])
 	{
 		printf("%s\n", argv[0]);
 	}
-	  return 0;
+	return 0;
 }
