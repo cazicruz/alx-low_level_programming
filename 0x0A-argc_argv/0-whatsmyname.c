2bf01c6078371294bd65b06d4e2d31699435eef3
cazicruz <dcazicruz@icloud.com>
2022-07-22 17:09:18 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Main - Entry point of the prog
+ * Returns: returns success as 0
+ */
+
+int main (int argc, char *argv[])
+{
+	prinf("%s", argv[0]);
+	  return 0;
+}
