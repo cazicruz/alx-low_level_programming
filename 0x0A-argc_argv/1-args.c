eb1c5e6d6d8753b08153f7875c7f286270854d49
cazicruz <dcazicruz@icloud.com>
2022-07-22 17:41:08 -0700

    argv task1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point of the prog
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: returns success as 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv >= 0)
+	{
+		printf("%s\n", argc);
+	}
+	return (0);
+}
