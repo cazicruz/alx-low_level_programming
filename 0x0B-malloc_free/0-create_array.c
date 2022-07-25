1008b7442fe786a725a1cba3f1982cbf3d234c6e
cazicruz <dcazicruz@icloud.com>
2022-07-25 13:34:07 -0700

    task1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,6 +4,8 @@
 
 /**
  * create_array - Entry point
+ * size: size of array
+ * c: char to fill array with
  * Return: p
  */
 
@@ -12,7 +14,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	char *p = (char *)malloc(size);
-	for (i = 0; i < size; i++)
+
+	for (i = 0; i <= size; i++)
 	{
 		p[i] = c;
 	}
