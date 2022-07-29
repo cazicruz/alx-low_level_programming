7188c042a2e92a367e96b4cb37cc77fecc784244
cazicruz <dcazicruz@icloud.com>
2022-07-29 11:55:27 -0700

    task0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,17 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include "main.h"
+
 /**
+ * malloc_checked - Entry point
+ * @p: pointer param
+ * @b: inpute param
+ * Return: retuns a pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
 {
 
-	void *p = malloc(b);
+	void *p;
+
+	p = malloc(b);
 	if (p == NULL)
 	{
 		exit (98);
 	}
-	else
-		return (p);
+	return (p);
 }
