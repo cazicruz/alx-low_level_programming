36eaeb4d75c235489f5de3edf218e46b3f0cd7f8
cazicruz <dcazicruz@icloud.com>
2022-08-01 14:41:51 -0700

    task0

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,8 +8,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	struct dog;
 	struct dog *d;
 
-	d -> name = name;
-	d -> age = age;
-	d -> owner = owner;
+	d-> name = name;
+	d-> age = age;
+	d-> owner = owner;
 	return (0);
 }
