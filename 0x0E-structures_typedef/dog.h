e806849c3cf24a45972c45d7cb27c56e3a647e23
cazicruz <dcazicruz@icloud.com>
2022-08-01 14:06:28 -0700

    task0 struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef _DOG_H_
-#define _DOG_H_
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Entry point of code
@@ -11,9 +11,10 @@
 typedef struct dog 
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 } dog;
+
 /**
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
