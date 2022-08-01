3857915b4f854c1b637d665f9a27669f809b6846
cazicruz <dcazicruz@icloud.com>
2022-08-01 14:23:52 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,26 +2,27 @@
 #define DOG_H
 
 /**
- * struct dog - Entry point of code
- * @name: variables
- * @owner: vriable
- * @age: variable
+ * dog_t - Typedef for struct dog
  *
- * Description: struct called "dog" that stores its name, age,
  */
 
-struct dog 
+typedef struct dog dog_t;
+
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, age,
+ *and the name of its owner.
+ *
+ */
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
-
-/**
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-*/
-
+};
 #endif
