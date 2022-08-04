d8606b3c482fe98c8eca887c513af4f0090d6f38
cazicruz <dcazicruz@icloud.com>
2022-08-03 19:39:44 -0700

    task2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,9 +15,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array && size)
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
+	if (!array || !size)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
