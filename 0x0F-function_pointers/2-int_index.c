451e206335928edf37054c6f825068b41f5d0f03
cazicruz <dcazicruz@icloud.com>
2022-08-03 20:47:29 -0700

    task2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,14 +10,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (!size || !array)
 		return (-1);
-	if (size <= 0)
+	else if (size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) == 0)
 			return (i);
-		else if (i + 1 == size)
-			return (-1);
 	}
+	return (-1);
 }
