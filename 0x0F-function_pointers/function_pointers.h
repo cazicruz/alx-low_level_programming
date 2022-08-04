d7526def7188ea052e3efb4ba1331f1b8bb4ddf0
cazicruz <dcazicruz@icloud.com>
2022-08-03 20:29:06 -0700

    .h files

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -47,4 +47,7 @@ char *argstostr(int ac, char **av);
 char **strtow(char *str);
 /* ponters to array */
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif
