1b12e919d92873627467d5da55919b80bffdbec1
cazicruz <dcazicruz@icloud.com>
2022-08-03 20:35:39 -0700

    .h files

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,17 +6,17 @@
  * @array: input integer array
  * @size: size of the array
  * @action: pointer to the function
- *
  * Return: void
  */
-
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
 	if (!array || !action)
+	{
 		return;
+	}
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
