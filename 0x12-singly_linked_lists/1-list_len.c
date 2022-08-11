bd313a4fbd70842da3e7c724dabbfb381bb35224
cazicruz <dcazicruz@icloud.com>
2022-08-11 15:24:05 -0700

    task1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * print_list - Entry point 
+ * print_list - Entry point
  * @s: size parameter
+ * @h: pointer to the list_t list
  *
+ * Return: number of elements in h
  */
 
 size_t list_len(const list_t *h)
