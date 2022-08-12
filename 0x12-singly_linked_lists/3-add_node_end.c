a454008b297b193d967ef02037e5c13c9a7beee6
cazicruz <dcazicruz@icloud.com>
2022-08-12 02:36:39 -0700

    task3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node_end - Entry point
+ * @head: pointer to the list_t list
+ * @ptr: storage pointer
+ * @new: new node
+ * Return: number of elements in h
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *ptr;
+	unsigned int len = 0;
+
+	ptr = *head;
+	new = malloc(sizeof(list_t));
+	new->str = strdup(str);
+
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+	}
+	ptr->next = new;
+}
